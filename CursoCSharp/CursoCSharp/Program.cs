b6e466534b9090938f2d71b72328529b2b467f64
Lucas <lucasreinaldosilva@outlook.com>
2022-02-11 16:54:28 -0300

    Fundamentos Curso de C#

---
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar },
+                {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
+                {"Inferência - Fundamentos", Inferencia.Executar },
+                {"Interpolação - Fundamentos", Interpolacao.Executar },
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar },
+                {"Lendo Dados - Fundamentos", lendoDados.Executar },
+                {"Formatando Número - Fundamentos", FormatandoNumero.Exectuar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
+            }); ;
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
