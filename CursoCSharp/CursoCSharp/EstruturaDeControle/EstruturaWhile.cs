6b10e2b74e5fb265310184653ee8fa7c0c8b00c2
Lucas <lucasreinaldosilva@outlook.com>
2022-03-02 17:50:45 -0300

    Estruturas de Controle

---
+++ CursoCSharp/CursoCSharp/EstruturaDeControle/EstruturaWhile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaWhile
+    {
+        public static void Executar()
+        {
+            int palpite = 0;
+            Random random = new Random();
+
+            int numeroSecreto = random.Next(1, 16);
+            bool numeroEncontrado = false;
+            int tentativasRestante = 5;
+            int tentativas = 0;
+
+            while(tentativasRestante > 0 && !numeroEncontrado)
+            {
+                Console.WriteLine("Insira o seu palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativas++;
+                tentativasRestante--;
+
+                if (numeroSecreto == palpite)
+                {
+                    numeroEncontrado = true;
+                    var corAnterior = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Numero encontrado em {0} tentativas", tentativas);
+                    Console.BackgroundColor = corAnterior;
+                } else if (palpite > numeroSecreto)
+                {
+                    Console.WriteLine("Menor... Tente Novamente");
+                    Console.WriteLine("Tentativas Restantes: {0}", tentativasRestante);
+                } else
+                {
+                    Console.WriteLine("Maior... Tente Novamente");
+                    Console.WriteLine("Temtativas Restantes: {0}", tentativasRestante);
+                }
+            }
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+
+                //Fundamentos 
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar },
                 {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
@@ -21,7 +24,14 @@ namespace CursoCSharp {
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar },
                 {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar },
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar },
-                {"Operador Ternário - Fundamentos", OperadorTernario.Executar }
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar },
+
+                //Estrutura de Controle
+                {"Estrutura If - Estrutura de Controle", EstruturaIf.Executar },
+                {"Estrutura If/Else - Estrutura de Controle", EstruturaIfElse.Executar },
+                {"Estrutura If/Else/If - Estrutura de Controle", EstruturaIfElseIf.Executar },
+                {"Estrutura Switch - Estrutura de Controle", EstruturaSwitch.Executar },
+                {"Estrutura While - Estrutura de Controle", EstruturaWhile.Executar },
             }); ;
 
             central.SelecionarEExecutar();
